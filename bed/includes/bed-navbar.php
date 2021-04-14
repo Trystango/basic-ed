5bebdfe161d8ee7cb1300bb1be885a6a7074a2a2
Mary Ann Dantes <maryanndantes080298@gmail.com>
2021-04-14 09:57:16 +0800

    Update bed-navbar.php

--- bed/includes/bed-navbar.php
+++ bed/includes/bed-navbar.php
@@ -26,6 +26,12 @@
                         while ($row = mysqli_fetch_array($get_user)) {
                             echo '<img src="data:image/jpeg;base64,' . base64_encode($row['img']) . '" class="img-circle elevation-0 mb-3" alt="User Image" style="width: 35px; height: 35px;">';
                         }
+                    }                                         
+                  elseif ($_SESSION['role'] == "Teacher") {
+                        $get_user = mysqli_query($conn, "SELECT * FROM tbl_teachers WHERE teacher_id = '$teacher_id'");
+                        while ($row = mysqli_fetch_array($get_user)) {
+                            echo '<img src="data:image/jpeg;base64,' . base64_encode($row['img']) . '" class="img-circle elevation-0 mb-3" alt="User Image" style="width: 35px; height: 35px;">';
+                        }
                     } ?>
 
                 </div>
